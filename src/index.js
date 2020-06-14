feca7047b21f6116d94402dd88f1acddbe681049
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-14 02:24:07 -0300

    First

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const src = () => {
+  return <Text>It's it!</Text>;
+}
+
+export default src;

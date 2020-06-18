78efb25d2d58dd0c616fc8c68df858bfba9b7a38
EmersonOliveiraDev <emersonhaw@gmail.com>
2020-06-18 03:39:48 -0300

    Add and config initial route

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ import { Provider } from 'react-redux'
 import { StatusBar } from 'react-native'
 
 import { store, persistor } from './store'
-import Routes from './routes'
+import App from '~/App'
 
-export default function src() {
+export default function Index() {
   return (
-    <>
-      <Provider store={store}>
-        <PersistGate persistor={persistor}>
-          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-          <Routes />
-        </PersistGate>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <App />
+      </PersistGate>
+    </Provider>
   )
 }
